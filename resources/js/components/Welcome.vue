ac805b4e44a67f360828a86e5c5c9321430a3da1
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2022-05-13 23:59:25 +0100

    tested first category crud. All working. No authentication yet.

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>SPA CRUD App With Laravel And Vue</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
